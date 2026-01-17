7355fbe811d1ec2aebc501fe1fd1223f163ed569
djadrianodolceofficial-netizen <dj.adrianodolce.official@gmail.com>
2026-01-17 13:03:37 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,10 @@
-const CACHE = "adiconiq-cache-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./catalog.html",
-  "./cart.html",
-  "./account.html",
-  "./styles.css",
-  "./main.js",
-  "./manifest.webmanifest"
-];
+const CACHE_NAME = 'adiconiq-v1';
+const assets = ['/', '/index.html', '/styles.css', '/main.js', '/offline.html'];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{}));
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null))))
-  );
-});
-
-self.addEventListener("fetch", (e) => {
-  const req = e.request;
-  e.respondWith(
-    caches.match(req).then(cached => cached || fetch(req).catch(() => cached))
-  );
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
